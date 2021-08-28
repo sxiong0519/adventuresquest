b19eee1c19bb9d17b797c64a5a5014883f2dfa1f
Sue Xiong <sxiong0519@gmail.com>
2021-08-27 21:36:22 -0500

    play again option

--- Quest/Program.cs
+++ Quest/Program.cs
@@ -57,26 +57,43 @@ namespace Quest
                 guessRandom,
                 favoriteBeatle
             };
-
-            // Loop through all the challenges and subject the Adventurer to them
-            foreach (Challenge challenge in challenges)
+            goOnAnAdventure();
+            void goOnAnAdventure()
             {
-                challenge.RunChallenge(theAdventurer);
+                // Loop through all the challenges and subject the Adventurer to them
+                foreach (Challenge challenge in challenges)
+                {
+                    challenge.RunChallenge(theAdventurer);
+                }
+                // This code examines how Awesome the Adventurer is after completing the challenges
+                // And praises or humiliates them accordingly
+                if (theAdventurer.Awesomeness >= maxAwesomeness)
+                {
+                    Console.WriteLine("YOU DID IT! You are truly awesome!");
+                }
+                else if (theAdventurer.Awesomeness <= minAwesomeness)
+                {
+                    Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
+                }
+                else
+                {
+                    Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                }
+                playAgain();
             }
 
-            // This code examines how Awesome the Adventurer is after completing the challenges
-            // And praises or humiliates them accordingly
-            if (theAdventurer.Awesomeness >= maxAwesomeness)
+            void playAgain()
             {
-                Console.WriteLine("YOU DID IT! You are truly awesome!");
-            }
-            else if (theAdventurer.Awesomeness <= minAwesomeness)
-            {
-                Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
-            }
-            else
-            {
-                Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                Console.WriteLine("Would you like to play again? Y/N");
+                string playAgain = Console.ReadLine().ToLower();
+                if (playAgain == "y" )
+                {
+                    goOnAnAdventure();
+                }
+                else 
+                {
+                    Console.WriteLine("Thank you for playing! See ya.");
+                }
             }
         }
     }
