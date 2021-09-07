54b13812bc74200b49d59be5c872b4a969441ad7
Sue Xiong <sxiong0519@gmail.com>
2021-09-07 10:55:02 -0500

    phase 7 completed

--- Quest/Robe.cs
+++ Quest/Robe.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 namespace Quest
 {
     public class Robe 
-    {
+    {   
+        //mutable properties: get; set;
         public static List<string> ColorOfTheRobe { get; set; }
         public static int Length { get; set; }
         
